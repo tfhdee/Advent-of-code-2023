d291ff092f15a8c877dbf761a77f4e3fe12dfde6
ThomaDee <thomasfdee95@gmail.com>
2024-09-14 21:55:16 +0100

    day 1 part 2

--- AOC_2023.R
+++ AOC_2023.R
@@ -1,6 +1,6 @@
 
 # Install packages, load libraries
-install.packages("tidyverse")
+#install.packages("tidyverse")
 library(dplyr)
 library(stringr)
 
@@ -16,3 +16,27 @@ day_1_data_task1 <- day_1_data %>%
 
 # sum of calibration values
 sum(day_1_data_task1$calibration_value) #55488
+
+# Day 2-------------------------------------------------------------------------
+
+day_1_part_2 <- day_1_data %>%
+  mutate(day_one_alt = day_one %>%
+           gsub("one", "o1e", .) %>%
+           gsub("two", "t2o", .) %>%
+           gsub("three", "t3e", .) %>%
+           gsub("four", "f4r", .) %>%
+           gsub("five", "f5e", .) %>%
+           gsub("six", "s6x", .) %>%
+           gsub("seven", "s7n", .) %>%
+           gsub("eight", "e8t", .) %>%
+           gsub("nine", "n9e", .))
+           
+           
+# Extract first digit
+day_1_part_2_answer <- day_1_part_2 %>% 
+  mutate(first_digit = str_extract(day_one_alt, "[0-9]"),
+         last_digit = str_extract(day_one_alt, "[0-9](?!.*[0-9])"),
+         calibration_value = as.numeric(paste0(first_digit, last_digit)))
+
+# sum of calibration values
+sum(day_1_part_2_answer$calibration_value) #55614
